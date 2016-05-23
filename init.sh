ce351b08eb4114c5b44f9b00ff67d598a7b67eb3
Oleksandr Lisovyi <sansan14@mail.ru>
2016-05-23 03:43:50 +0300

    sd

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/oleksandr/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
